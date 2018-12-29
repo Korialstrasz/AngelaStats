490d9bc68b02898b633ddf5c3fe882b2792476d7
Korialstrasz <knight-saber@gmx.de>
2018-12-29 23:06:23 +0100

    fix search

--- src/app/scene-overview/scene-overview.component.ts
+++ src/app/scene-overview/scene-overview.component.ts
@@ -46,7 +46,7 @@ export class SceneOverviewComponent {
   }
 
   contains(value: string, contain: string): boolean {
-    return value.toLowerCase().indexOf(contain.toLowerCase()) > 0
+    return value.toLowerCase().indexOf(contain.toLowerCase()) >= 0
   }
 
 
