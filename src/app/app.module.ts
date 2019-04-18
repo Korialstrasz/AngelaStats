edd84f005737edd7d5d006c3dbc18bd0faaca110
Korialstrasz <knight-saber@gmx.de>
2019-04-18 16:48:07 +0200

    fix project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonToggleModule, MatCheckboxModule, MatRadioModule} from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {SceneEntryComponent} from './scene-entry/scene-entry.component';
 import {SceneListComponent} from './scene-list/scene-list.component';
 import {OverviewComponent} from './overview/overview.component';
 import {StatisticOverviewComponent} from './statistic-overview/statistic-overview.component';
-import {DurationPipe} from './shared/duration.pipe';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MomentPipe} from './shared/moment.pipe';
-import {EncodeUriPipe} from './shared/ecodeUri.pipe';
-import { DvdListComponent } from './dvd-list/dvd-list.component';
-import { DvdEntryComponent } from './dvd-entry/dvd-entry.component';
+import {DurationPipe} from './shared/pipes/duration.pipe';
+import {MomentPipe} from './shared/pipes/moment.pipe';
+import {EncodeUriPipe} from './shared/pipes/ecodeUri.pipe';
+import {DvdListComponent} from './dvd-list/dvd-list.component';
+import {DvdEntryComponent} from './dvd-entry/dvd-entry.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,7 @@ import { DvdEntryComponent } from './dvd-entry/dvd-entry.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     MatInputModule,
     MatCheckboxModule,
     MatRadioModule,
